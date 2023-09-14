a0fd9b6b5d337f5f56fa294bedf4a686540559ba
iriseel <sirileefilekeeping@gmail.com>
2023-09-14 15:42:25 -0400

    added IP address to ip.md

--- script.sh
+++ script.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-echo hostname -I
+ip_address= hostname -I
+echo "current IP address is: $ip_address"
 hostname -I > ~/git/334/raspberrypi/ip.md
 git add .
 git commit -m "added IP address to ip.md"
