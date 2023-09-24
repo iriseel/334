5d3a6a3174ee70f39776a9917aa9b53092e0461b
iriseel <sirileefilekeeping@gmail.com>
2023-09-24 18:31:15 -0400

    added IP address to ip.md

--- get_ip.sh
+++ get_ip.sh
@@ -6,3 +6,5 @@ hostname -I > ~/git/334/raspberrypi/ip.md
 git add .
 git commit -m "added IP address to ip.md"
 git push
+
+scp ~/git/334/raspberrypi/ip.md sirilee@172.27.111.151:~/Desktop
---
+++ scp_ip.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+
+scp ~/git/334/raspberrypi/ip.md sirilee@172.26.52.2:~/Desktop
